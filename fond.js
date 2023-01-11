b425f3c4864156f4a98dbf20ae50b7993a59f358
DiniBis <106832577+DiniBis@users.noreply.github.com>
2023-01-11 19:49:43 +0100

    Add files via upload

---
+++ fond.js
@@ -0,0 +1,10 @@
+window.onclick = myFunction;
+
+function myFunction() {
+  document.getElementById("carte").style.backgroundColor = "#333"
+  document.getElementById("blanc").style.color = "#f1f1f1"
+  
+}
+
+///à ajouter après le body
+///<script src="fond.js"></script>
